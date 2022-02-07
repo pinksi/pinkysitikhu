ad821d19b48627d0bdccac1c7e7ba547ad2ecde4
pinksi <pinky.sitikhu524@gmail.com>
2022-02-07 08:20:52 -0600

    creates new intro page

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello 🌎");
+
+const btn = document.querySelector("button"); // Get the button from the page
+// Detect clicks on the button
+if (btn) {
+  btn.onclick = function() {
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+  };
+}
